f6d88861eb3117b772bc3de8ac0d1f6568956bdd
jaikugan <kuganpositive@gmail.com>
2026-07-12 11:10:34 +0530

    formatted

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,11 +12,11 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
 
-  vpc_id                  = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-  cidr_block              = var.subnet_cidr
+  cidr_block = var.subnet_cidr
 
-  availability_zone       = var.availability_zone
+  availability_zone = var.availability_zone
 
   map_public_ip_on_launch = true
 
